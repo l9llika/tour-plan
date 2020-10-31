d38ffdb8ca34e2d5f7eafada7e7175223ed3392c
L9LLIKA <L9LLIKA@yandex.ru>
2020-10-31 22:40:15 +0300

    create: validation in forms

--- js/main.js
+++ js/main.js
@@ -52,4 +52,23 @@ function closeModal(event) {
   modalOverlay.removeClass('modal__overlay--visible');
   modalDialog.removeClass('modal__dialog--visible');
 }
+  // Обработка форм
+    $('.form').each(function () {
+      $(this).validate({
+        errorClass: "invalid",
+        messages: {
+          name: {
+            required: "Please enter your name",
+            minlength: "Name at least 2 letters",
+          },
+          email: {
+            required: "We need your email address to contact you",
+            email: "Your email address must be in the format of name@domain.com",
+          },
+          phone: {
+            required: "Enter your phone number",
+          },
+        },
+      });
+    });
 });
